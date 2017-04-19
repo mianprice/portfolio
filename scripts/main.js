87b69052d0ec9a4d7c762ef1dc682209dd671284
Ian Price <michael.ian.price@gmail.com>
2017-04-19 17:26:27 -0400

    Styling revised

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 var changed = false;
 
 $(function() {
+  var footer_height = $(window).height() - $('#contact').height() - $('#navbar').height() - ($('#c-info').height() + parseInt($('#c-info').css('padding-bottom').replace('px','')) + parseInt($('#c-info').css('padding-top').replace('px','')));
+  $('#post-contact').height(footer_height);
   $("#nav").onePageNav({
     currentClass: 'current',
     changeHash: false,
