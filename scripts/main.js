f5834f3fe10c93f3cd0143ecb334efcc70c0e0f8
Ian Price <michael.ian.price@gmail.com>
2017-04-20 08:26:33 -0400

    Styling nearly complete, just need icons for links at the bottom.

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,21 @@
 var changed = false;
 
 $(function() {
-  var footer_height = $(window).height() - $('#contact').height() - $('#navbar').height() - ($('#c-info').height() + parseInt($('#c-info').css('padding-bottom').replace('px','')) + parseInt($('#c-info').css('padding-top').replace('px','')));
-  $('#post-contact').height(footer_height);
+  var footer_height = $(window).height() - $('#contact').outerHeight() - $('#navbar').height() - $('#c-info').outerHeight();
+  $('.spacer').height(footer_height);
   $("#nav").onePageNav({
     currentClass: 'current',
     changeHash: false,
     scrollSpeed: 600,
     scrollThreshold: 0.5,
+    end: function() {
+      if (document.body.scrollTop === 0) {
+        $('.current').toggleClass('.current');
+      }
+    }
   });
+  $("#back-top").on('click', (e) => {
+    e.preventDefault();
+    $(".nav-title").trigger('click');
+  })
 });
