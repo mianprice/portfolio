53a1244a5768dbf1473825cd5661bc368cfb8166
mianprice <michael.ian.price@gmail.com>
2017-04-24 19:59:20 +0000

    fighting with git

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 var changed = false;
 
 $(window).load(() => {
-  var x = $(window).width() > 750 ? 0 : 0;
-  var h = $(window).height() - $('#contact').outerHeight() - $('#c-info').outerHeight() - $('#nav-blur').height();
-  if ($(window).width() < 750) {
+  var x = $(document).width() > 750 ? 0 : 0;
+  var h = $(document).height() - $('#contact').outerHeight() - $('#c-info').outerHeight() - $('#nav-blur').height();
+  if ($(document).width() < 750) {
     h = h - $('#c-info').outerHeight();
   }
   h = h <= 0 ? 300 : h;
