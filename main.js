9eaf6af1cf220afa0cc2663512d657e6ac04b8f1
mianprice <michael.ian.price@gmail.com>
2019-08-09 00:57:30 -0400

    background shapes and scroll behavior handled

--- main.js
+++ main.js
@@ -1,80 +1,60 @@
 console.log("this script executes");
 
-var d1 = document.getElementById('layer1');
-var d2 = document.getElementById('layer1');
-var d3 = document.getElementById('layer1');
-var d4 = document.getElementById('layer1');
+var d = [document.getElementById('layer1'), document.getElementById('layer2'), document.getElementById('layer3'), document.getElementById('layer4')];
 
-for (var i = 0; i < 50; i++) {
-    var r1 = getRandomInt(0, 255),
-        r2 = getRandomInt(0, 255),
-        r3 = getRandomInt(0, 255),
-        r4 = getRandomInt(0, 255),
-        g1 = getRandomInt(0, 255),
-        g2 = getRandomInt(0, 255),
-        g3 = getRandomInt(0, 255),
-        g4 = getRandomInt(0, 255),
-        b1 = getRandomInt(0, 255),
-        b2 = getRandomInt(0, 255),
-        b3 = getRandomInt(0, 255),
-        b4 = getRandomInt(0, 255),
-        x1 = getRandomInt(0, 100),
-        x2 = getRandomInt(0, 100),
-        x3 = getRandomInt(0, 100),
-        x4 = getRandomInt(0, 100),
-        y1 = getRandomInt(0, 100),
-        y2 = getRandomInt(0, 100),
-        y3 = getRandomInt(0, 100),
-        y4 = getRandomInt(0, 100),
-        i1 = getRandomInt(0, 100),
-        i2 = getRandomInt(0, 100),
-        i3 = getRandomInt(0, 100),
-        i4 = getRandomInt(0, 100),
-        j1 = getRandomInt(0, 100),
-        j2 = getRandomInt(0, 100),
-        j3 = getRandomInt(0, 100),
-        j4 = getRandomInt(0, 100),
-        k1 = getRandomInt(0, 100),
-        k2 = getRandomInt(0, 100),
-        k3 = getRandomInt(0, 100),
-        k4 = getRandomInt(0, 100),
-        l1 = getRandomInt(0, 100),
-        l2 = getRandomInt(0, 100),
-        l3 = getRandomInt(0, 100),
-        l4 = getRandomInt(0, 100),
-        m1 = getRandomInt(0, 100),
-        m2 = getRandomInt(0, 100),
-        m3 = getRandomInt(0, 100),
-        m4 = getRandomInt(0, 100),
-        n1 = getRandomInt(0, 100),
-        n2 = getRandomInt(0, 100),
-        n3 = getRandomInt(0, 100),
-        n4 = getRandomInt(0, 100);
+// colors should be r,g,b so they can be dropped into rgba() filters
+var colors = ['228,1,169,', '97,236,243,', '163,117,206,', '10,17,192,', '208,3,210,', '99,45,200,'];
 
-    var one = `<div style="background-color:rgb(${r1},${g1},${b1});clip-path:polygon(${i1}% ${l1}%, ${j1}% ${m1}%, ${k1}% ${n1}%);position:absolute;top: ${x1}%;left: ${y1}%;"></div>`;
-    var two = `<div style="background-color:rgb(${r2},${g2},${b2});clip-path:polygon(${i2}% ${l2}%, ${j2}% ${m2}%, ${k2}% ${n2}%);position:absolute;top: ${x2}%;left: ${y2}%;"></div>`;
-    var three = `<div style="background-color:rgb(${r3},${g3},${b3});clip-path:polygon(${i3}% ${l3}%, ${j3}% ${m3}%, ${k3}% ${n3}%);position:absolute;top: ${x3}%;left: ${y3}%;"></div>`;
-    var four = `<div style="background-color:rgb(${r4},${g4},${b4});clip-path:polygon(${i4}% ${l4}%, ${j4}% ${m4}%, ${k4}% ${n4}%);position:absolute;top: ${x4}%;left: ${y4}%;"></div>`;
 
-    console.log(one);
-    console.log(two);
-    console.log(three);
-    console.log(four);
+for (var i = 0; i < 10; i++) {
+    var c = [...Array(4)].map(x => {
+            var colorCopy = [...colors];
+            return [...Array(2)].map(x => colorCopy.splice(getRandomInt(0,colorCopy.length-1),1));
+        }),
+        deg = [...Array(4)].map(x => getRandomInt(0,359)),
+        x1 = [...Array(4)].map(x => getRandomInt(0,100)),
+        x2 = [...Array(4)].map(x => getRandomInt(0,100)),
+        y1 = [...Array(4)].map(x => getRandomInt(0,50)),
+        y2 = [...Array(4)].map(x => getRandomInt(50,100)),
+        lr = [...Array(4)].map(x => getRandomInt(0,1)),
+        bt = [...Array(4)].map(x => getRandomInt(0,1));
+        s = [1,1,1,1],
+        p = [...Array(4)].map(x => getRandomTriangle());
 
-    d1.innerHTML += one;
-    d2.innerHTML += two;
-    d3.innerHTML += three;
-    d4.innerHTML += four;
+    for (var j = 0; j < 4; j++) {
+        t1 = 1.0-(y1[j]<50?(y1[j]/50.0):((y1[j]-100.0)/-50.0));
+        t2 = 1.0-(y2[j]<50?(y2[j]/50.0):((y2[j]-100.0)/-50.0));
+        console.log(`t: ${t1} => y: ${y1[j]}`);
+        console.log(`t: ${t2} => y: ${y2[j]}`);
+        var insert1 = `<div style="border:1px solid rgba(255,255,255${t1}); position:absolute;${bt[j] ? 'bottom' : 'top'}: ${x1[j]}%;${lr[j] ? 'left' : 'right'}: ${y1[j]}%;"><div style="width:${50*s[j]+100}px;height:${50*s[j]+100}px;background:linear-gradient(${deg[j]}deg,rgba(${c[j][0]}${t1}),rgba(${c[j][1]}${t1}));clip-path:polygon(${p[j][0]}% ${p[j][1]}%, ${p[j][2]}% ${p[j][3]}%, ${p[j][4]}% ${p[j][5]}%);position:absolute;${bt[j] ? 'bottom' : 'top'}: ${x1[j]}%;${lr[j] ? 'left' : 'right'}: ${y1[j]}%;"></div></div>`;
+        var insert2 = `<div style="border:1px solid rgba(255,255,255${t2}); position:absolute;${bt[j] ? 'bottom' : 'top'}: ${x2[j]}%;${lr[j] ? 'left' : 'right'}: ${y2[j]}%;"><div style="width:${50*s[j]+100}px;height:${50*s[j]+100}px;background:linear-gradient(${deg[j]}deg,rgba(${c[j][0]}${t2}),rgba(${c[j][1]}${t2}));clip-path:polygon(${p[j][0]}% ${p[j][1]}%, ${p[j][2]}% ${p[j][3]}%, ${p[j][4]}% ${p[j][5]}%);position:absolute;${bt[j] ? 'bottom' : 'top'}: ${x2[j]}%;${lr[j] ? 'left' : 'right'}: ${y2[j]}%;"></div></div>`;
+        // console.log(insert1);
+        // console.log(insert2);
+        d[j].innerHTML += insert1 + insert2;
+    }
 }
 
 window.addEventListener('scroll', () => {
     let parent = document.getElementById('sprites');
     let children = parent.getElementsByTagName('div');
     for(let i = 0; i < children.length; i++) {
-      children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
+        children[i].style.transform = 'translateY(-' + (window.pageYOffset * (i+1) / (children.length*4)) + 'px)';
     }
- }, false)
+}, false)
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomTriangle() {
+    var x1, x2, x3, y1, y2, y3;
+
+    x1 = getRandomInt(0,100);
+    y1 = getRandomInt(0,25);
+    x2 = getRandomInt(0,60);
+    y2 = x1 < 50 ? getRandomInt(30, 100) : getRandomInt(0, 70);
+    x3 = getRandomInt(40,100);
+    y3 = x1 > 50 ? getRandomInt(30, 100) : getRandomInt(0, 70);
+
+    return [x1, y1, x2, y2, x3, y3];
 }
